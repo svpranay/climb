bafb317dc88c9f8896b4f8f85521b1c1ae825de1
pranay <svpranay@gmail.com>
2016-06-20 00:27:19 -0700

    [91. Decode Ways]

---
+++ 91-Decode-Ways/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int numDecodings(String s) {
+        int size = s.length();
+        if (size ==0) return 0;
+        int[] a = new int[size+1];
+        for(int i=0;i<=size;i++) {
+            a[i] = 0;
+        }
+        //if (s[size-1] )
+        a[size] = 1;
+        if (s.charAt(size-1) != '0') 
+            a[size-1] = 1;
+        for(int i=size-2;i>=0;i--) {
+            if((s.charAt(i) == '1') || (s.charAt(i) == '2' && s.charAt(i+1) >= '0' && s.charAt(i+1) <= '6' ) ) {
+                a[i] = a[i+2] + a[i+1];
+            } else if (s.charAt(i) == '0') {
+                // 0 9 
+                a[i] = 0;
+            } else {
+                a[i] = a[i+1];
+            }
+            System.out.println(i + " " + a[i]);
+        }
+        return a[0];
+    }
+}
